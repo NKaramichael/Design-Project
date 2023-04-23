de486eacbed83c1b8f88c59aa94feca6b57e84b8
nick-karamichael <127729535+nick-karamichael@users.noreply.github.com>
2023-04-23 11:09:49 +0200

    Created profile.html
    created profile.html where users are able to view their account details

--- firebase-init.js
+++ firebase-init.js
@@ -1,233 +1,233 @@
 // This document handles account queries such as signup/login/change username/change password
 //configuration for connecting to firebase database
 const firebaseConfig = {
-    apiKey: "AIzaSyDPhBs6YrLXQspg8krTemU6WdlArx4lNQ4",
-    authDomain: "pcgevaluation-49d75.firebaseapp.com",
-    databaseURL: "https://pcgevaluation-49d75-default-rtdb.firebaseio.com",
-    projectId: "pcgevaluation-49d75",
-    storageBucket: "pcgevaluation-49d75.appspot.com",
-    messagingSenderId: "369543877095",
-    appId: "1:369543877095:web:84e7d5c5fdb84dd72eed42"
-  };
+  apiKey: "AIzaSyDPhBs6YrLXQspg8krTemU6WdlArx4lNQ4",
+  authDomain: "pcgevaluation-49d75.firebaseapp.com",
+  databaseURL: "https://pcgevaluation-49d75-default-rtdb.firebaseio.com",
+  projectId: "pcgevaluation-49d75",
+  storageBucket: "pcgevaluation-49d75.appspot.com",
+  messagingSenderId: "369543877095",
+  appId: "1:369543877095:web:84e7d5c5fdb84dd72eed42"
+};
 
-  //initialise firebase
-  firebase.initializeApp(firebaseConfig);
+//initialise firebase
+firebase.initializeApp(firebaseConfig);
 
-  //create reference for DB
-  var contactFormDB = firebase.database().ref('contactForm')
+//create reference for DB
+var contactFormDB = firebase.database().ref('contactForm')
 
-  //listen to if the signup button is pressed or login button, redirect to respective methods
-  function signUp(){
-    document.getElementById('signupForm').addEventListener('submit', submitSignUp);
-  }
+//listen to if the signup button is pressed or login button, redirect to respective methods
+function signUp() {
+  document.getElementById('signupForm').addEventListener('submit', submitSignUp);
+}
 
-  function login(){
-    document.getElementById('loginForm').addEventListener('submit', submitLogin);
-  }
+function login() {
+  document.getElementById('loginForm').addEventListener('submit', submitLogin);
+}
 
-  // Listen to if form button submitChangePassword and change them in the database
-  function changePassword(){
-    document.getElementById('Form').addEventListener('submit', submitChangePassword);
-  }
-  // Submits changed password to the database and replaces the 
-  function submitChangePassword(e){
-    e.preventDefault();
-    var newPassword = getElementVal('password'); //Find the password of the current user through their session info
-    var email = sessionStorage.getItem('email');
-    
-    firebase.database().ref("contactForm").orderByChild("email").equalTo(email).once("value", function(snapshot) {
-      snapshot.forEach(function(childSnapshot) {
-        const childKey = childSnapshot.key;
-        const childData = childSnapshot.val();
+// Listen to if form button submitChangePassword and change them in the database
+function changePassword() {
+  document.getElementById('Form').addEventListener('submit', submitChangePassword);
+}
+// Submits changed password to the database and replaces the 
+function submitChangePassword(e) {
+  e.preventDefault();
+  var newPassword = getElementVal('password'); //Find the password of the current user through their session info
+  var email = sessionStorage.getItem('email');
 
-        // update the password value of the node with the matching email address
-        var updates = {
-          password : newPassword
-        }
+  firebase.database().ref("contactForm").orderByChild("email").equalTo(email).once("value", function (snapshot) {
+    snapshot.forEach(function (childSnapshot) {
+      const childKey = childSnapshot.key;
+      const childData = childSnapshot.val();
 
-        firebase.database().ref("contactForm/" + childKey).update(updates);
-        alert("Password Successfully Changed!");
-        if(sessionStorage.getItem('role') == "Researcher"){
-          sessionStorage.setItem('password', newPassword);
-          window.location.replace("./currentResearcherBoard.html");
-        }
-        else{
-          sessionStorage.setItem('password', newPassword);
-          window.location.replace("./currentUserBoard.html");
+      // update the password value of the node with the matching email address
+      var updates = {
+        password: newPassword
+      }
+
+      firebase.database().ref("contactForm/" + childKey).update(updates);
+      alert("Password Successfully Changed!");
+      if (sessionStorage.getItem('role') == "Researcher") {
+        sessionStorage.setItem('password', newPassword);
+        window.location.replace("./currentResearcherBoard.html");
+      }
+      else {
+        sessionStorage.setItem('password', newPassword);
+        window.location.replace("./currentUserBoard.html");
+      }
+    });
+  });
+
+}
+
+function submitLogin(e) {
+  e.preventDefault();
+
+  //get the values put into the form using getElementval and store in var's.
+  var email = getElementVal('email');
+  var password = getElementVal('password');
+
+  // Query the database to see if the email is already in use
+  contactFormDB.orderByChild("email").equalTo(email).once("value", (snapshot) => {
+    if (snapshot.exists()) {
+      // The email exists in the database, check if the password matches
+      snapshot.forEach((user) => {
+        const userData = user.val();
+        if (userData.password === password) {
+          // The password matches, log the user in
+          sendToDash(userData.email, userData.role);
+        } else {
+          // The password doesn't match, show an error message
+          alert("Incorrect password!");
         }
       });
-    });
+    } else {
+      // The email is not in use
+      alert("Email is not registered!");
+    }
+  });
+}
 
+//The main signup method
+function submitSignUp(e) {
+  e.preventDefault();
+
+  //get the values put into the form using getElementval and store in var's.
+  var email = getElementVal('email');
+  var password = getElementVal('password');
+  var role = '';
+  var checkbox = document.getElementById('role');
+
+  //sets role based on the state of the checkbox
+  if (checkbox.checked) {
+    role = "Researcher";
+  }
+  else {
+    role = "User";
   }
 
-  function submitLogin(e){
-    e.preventDefault();
-
-    //get the values put into the form using getElementval and store in var's.
-    var email = getElementVal('email');
-    var password = getElementVal('password');
-
-    // Query the database to see if the email is already in use
-    contactFormDB.orderByChild("email").equalTo(email).once("value", (snapshot) => {
-      if (snapshot.exists()) {
-        // The email exists in the database, check if the password matches
-        snapshot.forEach((user) => {
-          const userData = user.val();
-          if (userData.password === password) {
-            // The password matches, log the user in
-            sendToDash(userData.email,userData.role);
-          } else {
-            // The password doesn't match, show an error message
-            alert("Incorrect password!");
-          }
-        });
-      } else {
-        // The email is not in use
-        alert("Email is not registered!");
-      }
-    });
+  //verify that email and password are in correct format
+  let outputPass = "Invalid password. Address the following:"
+  let passError = false
+  let outputEmail = ""
+  let emailError = false
+  let passwordValidationArr = validate_password(password);
+  if (!validate_email(email)) {
+    emailError = true
+    outputEmail += "Invalid email. Ensure your email is of a valid domain and that it has not been used to create an account before"
+  }
+  if (!passwordValidationArr[0]) {
+    passError = true
+    outputPass += "\n- Password is not 8 characters long"
+  }
+  if (!passwordValidationArr[1]) {
+    passError = true
+    outputPass += "\n- Password does not contain a lower case letter"
+  }
+  if (!passwordValidationArr[2]) {
+    passError = true
+    outputPass += "\n- Password does not contain a capital letter"
+  }
+  if (!passwordValidationArr[3]) {
+    passError = true
+    outputPass += "\n- Password does not contain a number"
   }
 
-  //The main signup method
-  function submitSignUp(e){
-    e.preventDefault();
+  //alert("signing up with these details: " + "\n" + email + "\n" + password + "\n" + role)
+  // Query the database to see if the email is already in use
+  contactFormDB.orderByChild("email").equalTo(email).once("value", (snapshot) => {
+    if (snapshot.exists()) {
+      // The email is already in use
+      alert("Email is already in use!");
+    } else if (emailError && passError) {
+      alert(outputEmail + ".\n\n" + outputPass)
+    } else if (emailError && !passError) {
+      alert(outputEmail)
+    } else if (!emailError && passError) {
+      alert(outputPass)
+    } else {
+      //Request that the data is saved in the Database using the saveMessage function.
+      saveMessages(email, password, role);
+    }
+  });
+}
 
-    //get the values put into the form using getElementval and store in var's.
-    var email = getElementVal('email');
-    var password = getElementVal('password');
-    var role = '';
-    var checkbox = document.getElementById('role');
+const sendToDash = (email, role) => {
+  //Redirecting visitor to their specific GUI dashboard based on "Researcher" or "User".
+  if (role == "Researcher") {
+    sessionStorage.removeItem('email');
+    sessionStorage.removeItem('password');
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
 
-    //sets role based on the state of the checkbox
-    if(checkbox.checked){
-      role = "Researcher";
-    }
-    else{
-      role = "User";
-    }
+    sessionStorage.setItem('email', email);
+    sessionStorage.setItem('password', password);
+    sessionStorage.setItem('role', role);
+    window.location.replace("./currentResearcherBoard.html");
+  }
+  else {
+    sessionStorage.removeItem('email');
+    sessionStorage.removeItem('password');
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
 
-    //verify that email and password are in correct format
-    let outputPass = "Invalid password. Address the following:"
-    let passError = false
-    let outputEmail = ""
-    let emailError = false
-    let passwordValidationArr = validate_password(password);
-    if(!validate_email(email)){
-      emailError = true
-      outputEmail += "Invalid email. Ensure your email is of a valid domain and that it has not been used to create an account before"
-    }
-    if (!passwordValidationArr[0]){
-      passError = true
-      outputPass += "\n- Password is not 8 characters long"
-    }
-    if (!passwordValidationArr[1]){
-      passError = true
-      outputPass += "\n- Password does not contain a lower case letter"
-    }
-    if (!passwordValidationArr[2]){
-      passError = true
-      outputPass += "\n- Password does not contain a capital letter"
-    }
-    if (!passwordValidationArr[3]){
-      passError = true
-      outputPass += "\n- Password does not contain a number"
-    }
-    
-    //alert("signing up with these details: " + "\n" + email + "\n" + password + "\n" + role)
-    // Query the database to see if the email is already in use
-    contactFormDB.orderByChild("email").equalTo(email).once("value", (snapshot) => {
-      if (snapshot.exists()) {
-        // The email is already in use
-        alert("Email is already in use!");
-      } else if (emailError && passError){
-        alert(outputEmail + ".\n\n" + outputPass)
-      } else if (emailError && !passError){
-        alert(outputEmail)
-      } else if (!emailError && passError){
-        alert(outputPass)
-      } else {
-        //Request that the data is saved in the Database using the saveMessage function.
-        saveMessages(email,password,role);
-      }
-    });
+    sessionStorage.setItem('email', email);
+    sessionStorage.setItem('password', password);
+    sessionStorage.setItem('role', role);
+    window.location.replace("./newUserBoard.html");
   }
 
-  const sendToDash = (email,role) => {
-    //Redirecting visitor to their specific GUI dashboard based on "Researcher" or "User".
-    if(role == "Researcher"){
-      sessionStorage.removeItem('email');
-      sessionStorage.removeItem('password');
-      const email = document.getElementById('email').value;
-      const password = document.getElementById('password').value;
+};
 
-      sessionStorage.setItem('email', email);
-      sessionStorage.setItem('password', password);
-      sessionStorage.setItem('role', role);
-      window.location.replace("./currentResearcherBoard.html");
-    }
-    else{
-      sessionStorage.removeItem('email');
-      sessionStorage.removeItem('password');
-      const email = document.getElementById('email').value;
-      const password = document.getElementById('password').value;
+//add user credentials to firebase database
+const saveMessages = (email, password, role) => {
+  var newContactForm = contactFormDB.push();
 
-      sessionStorage.setItem('email', email);
-      sessionStorage.setItem('password', password);
-      sessionStorage.setItem('role', role);
-      window.location.replace("./newUserBoard.html");
-    }
-    
-  };
+  newContactForm.set({
+    email: email,
+    password: password,
+    role: role,
+  });
 
-  //add user credentials to firebase database
-  const saveMessages = (email,password,role) => {
-    var newContactForm = contactFormDB.push();
-    
-    newContactForm.set({
-      email : email,
-      password : password,
-      role : role,
-    });
-    
-    //Redirecting visitor to their specific GUI dashboard based on "Researcher" or "User".
-    if(role == "Researcher"){
-      sessionStorage.removeItem('email');
-      sessionStorage.removeItem('password');
-      const email = document.getElementById('email').value;
-      const password = document.getElementById('password').value
+  //Redirecting visitor to their specific GUI dashboard based on "Researcher" or "User".
+  if (role == "Researcher") {
+    sessionStorage.removeItem('email');
+    sessionStorage.removeItem('password');
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value
 
-      sessionStorage.setItem('email', email);
-      sessionStorage.setItem('password', password);
-      window.location.replace("./currentResearcherBoard.html");
-    }
-    else{
-      sessionStorage.removeItem('email');
-      sessionStorage.removeItem('password');
-      const email = document.getElementById('email').value;
-      const password = document.getElementById('password').value
+    sessionStorage.setItem('email', email);
+    sessionStorage.setItem('password', password);
+    window.location.replace("./currentResearcherBoard.html");
+  }
+  else {
+    sessionStorage.removeItem('email');
+    sessionStorage.removeItem('password');
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value
 
-      sessionStorage.setItem('email', email);
-      sessionStorage.setItem('password', password);
-      window.location.replace("./newUserBoard.html");
-    }
-    
-  };
+    sessionStorage.setItem('email', email);
+    sessionStorage.setItem('password', password);
+    window.location.replace("./newUserBoard.html");
+  }
 
-  const getElementVal = (id) => {
-    return document.getElementById(id).value;
-  };
+};
+
+const getElementVal = (id) => {
+  return document.getElementById(id).value;
+};
 
 //checks if email is a valid format
-function validate_email(email){
+function validate_email(email) {
   let expression = /^[^@]+@\w+(\.\w+)+\w$/;
   return expression.test(email) == true;
 }
 
 //checks if password is longer than 7 characters, and has at least one capital letter and number
-function validate_password(password){
+function validate_password(password) {
   let lengthValid = false;
 
-  if (password.length >= 8){
+  if (password.length >= 8) {
     lengthValid = true;
   }
 
@@ -241,4 +241,30 @@ function validate_password(password){
   const hasNumber = /\d/.test(password);
 
   return [lengthValid, hasLowerCaseLetter, hasCapitalLetter, hasNumber];
+}
+//Code to display email and password in profile
+function displayE() {
+  return sessionStorage.getItem("email")
+}
+function displayP() {
+  return sessionStorage.getItem("password")
+}
+function account() {
+  var myDiv1 = document.getElementById("email");
+  var myDiv2 = document.getElementById("password");
+  if (myDiv1.textContent != "" && myDiv2.textContent != "") {
+    myDiv1.textContent = "";
+    myDiv2.textContent = "";
+    var Div3 = document.getElementById("AccountBtn");
+    Div3.textContent = "View Account Details"
+  } else {
+    var myDiv1 = document.getElementById("email");
+    mail = displayE();
+    myDiv1.textContent = mail;
+    var myDiv2 = document.getElementById("password");
+    pass = displayP();
+    myDiv2.textContent = pass;
+    var Div3 = document.getElementById("AccountBtn");
+    Div3.textContent = "Hide Account Details"
+  }
 }
