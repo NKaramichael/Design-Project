f0aa5366f8c8e6d6034d55488d4b685b98caed0a
gabrieldrew <100573616+gabrieldrew@users.noreply.github.com>
2023-05-13 23:03:53 +0200

    Add database currentQuizzes functionality
    Added functionality to fetch user's current quizzes from database and then display the quizzes from data in the database.

--- 4-JavaScript/firebase-bucket.js
+++ 4-JavaScript/firebase-bucket.js
@@ -9,21 +9,103 @@ const firebaseConfig = {
     messagingSenderId: "369543877095",
     appId: "1:369543877095:web:84e7d5c5fdb84dd72eed42"
 };
-  
+
+
 //initialise firebase
 firebase.initializeApp(firebaseConfig);
 // Get a reference to the storage service, which is used to create references in your storage bucket
 var storage = firebase.storage();
 // Create a storage reference from our storage service
 var storageRef = storage.ref();
-// Create a child reference
-var thumbsRef = storageRef.child('Levels/Thumbnails/testimage.jpeg').getDownloadURL()
-.then((url) => {
-  // `url` is the download URL
-  // inserted into an <img> element
-  var img = document.getElementById('image1');
-  img.setAttribute('src', url);
-})
-.catch((error) => {
-  // Handle any errors
-});
+
+//firestore reference
+const db = firebase.firestore();
+var UserFirestore = db.collection('Users');
+
+// Fetchg and diosplay the user's currently active quizzes
+function displayCurrentQuizzes() {
+  // var email = sessionStorage.getItem('email');
+  var email = 'meow10@catmail.com';
+  userRef = UserFirestore.doc(email);
+  // query database for array of currently active quizzes
+  userRef.get().then((doc) => {
+      if (doc.exists) {
+        quizRefs = (doc.data()['currentQuizzes']);
+        // loop through array and get info for each quiz to display
+        quizRefs.forEach((ref) => {
+          ref.get().then((doc) => {
+              if (doc.exists) {
+                // display the quiz block
+                createQuizBlock(doc.data());
+              } else {
+                  // The document doesn't exist
+                  console.log("No such document!");
+              }
+          }).catch((error) => {
+              // An error occurred while retrieving the document
+              console.log("Error getting document:", error);
+          });
+      });
+      } else {
+          // doc.data() will be undefined in this case
+          alert("No such document!");
+      }
+  }).catch((error) => {
+      alert("Error getting document:", error);
+  });
+};
+
+async function createQuizBlock(data) {
+  // define values for the box
+  const box = {
+    title: data['Title'],
+    description: data['Description']
+  };
+  // Get a reference to the container element
+  // Get the parent element to which the text boxes will be added
+  const parent = document.getElementById("text-boxes");
+
+  const textBox = document.createElement("div");
+  textBox.classList.add("text-box");
+  // get and set thumbnail link froim storage
+  parentDirec = 'Levels/Thumbnails/'
+  const image = document.createElement("img");
+  await storageRef.child(parentDirec+data['Thumbnail']).getDownloadURL()
+  .then((url) => {
+    // `url` is the download URL\
+    image.setAttribute("src", url);
+  })
+  .catch((error) => {
+    // Handle any errors
+    console.log(error)
+  });
+
+  const title = document.createElement("h2");
+  title.textContent = box.title;
+
+  const description = document.createElement("p");
+  description.textContent = box.description;
+
+  const openButton = document.createElement("button");
+  openButton.classList.add("button");
+  openButton.style.backgroundColor = "rgb(65, 239, 65)";
+  openButton.style.color = "white";
+  openButton.textContent = "Open";
+  openButton.addEventListener("click", openSurvey);
+
+  const removeButton = document.createElement("button");
+  removeButton.classList.add("button");
+  removeButton.style.backgroundColor = "rgb(241, 16, 16)";
+  removeButton.style.color = "white";
+  removeButton.textContent = "Remove";
+
+  // Add the elements to the text box
+  textBox.appendChild(image);
+  textBox.appendChild(openButton);
+  textBox.appendChild(removeButton);
+  textBox.appendChild(title);
+  textBox.appendChild(description);
+
+  // Add the text box to the parent element
+  parent.appendChild(textBox);
+};
