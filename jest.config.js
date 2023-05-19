8382bffcbabcaa43aebbffb2bcae4cefebdd5f20
Tristan Dos Remendos <tristan.remendos@gmail.com>
2023-05-19 09:10:36 +0200

    Firebase testing commands

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
     collectCoverage: true,
     coverageReporters: ['text', 'cobertura'],
+    testEnvironment: 'node',
+    transform: {
+        '^.+\\.js$': 'babel-jest',
+    },
 }
