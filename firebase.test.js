952d49e9b94038da16430293c0dd4529f7d5d7e0
Tristan Dos Remendos <tristan.remendos@gmail.com>
2023-05-19 09:19:31 +0200

    Firebase testing files

---
+++ firebase.test.js
@@ -0,0 +1,12 @@
+const myFirebaseFunction = require('../myFirebaseFunction');
+
+test('Test myFirebaseFunction', async () => {
+  // Mock Firebase dependencies or initialize Firebase emulator
+  // to create a controlled testing environment
+
+  // Call your Firebase function or execute the code that interacts with Firebase
+
+  // Make assertions to check the expected behavior or outcomes
+
+  // Clean up any test-specific resources or data
+});
