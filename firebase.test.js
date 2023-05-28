a79b504e9eb9d00e1bb655b33ea48382fbc91585
Tristan Dos Remendos <tristan.remendos@gmail.com>
2023-05-28 16:04:41 +0200

    Update firebase.test.js

--- firebase.test.js
+++ firebase.test.js
@@ -1,19 +1,19 @@
 let {
-    selectImage, greyOutImage, submitToEval, listAll
-  } = require('./4-JavaScript/firebase-selectImages.js');
-  
+  selectImage, greyOutImage, submitToEval, listAll
+} = require('./4-JavaScript/firebase-selectImages.js');
+
 test('selectImage', async () => {
   // Mock Firebase dependencies or initialize Firebase emulator
   // to create a controlled testing environment
 
-    expect(1).toBe(1);
+  expect(1).toBe(1);
 
   // Call your Firebase function or execute the code that interacts with Firebase
 
-    expect(typeof (listAll())).toBe("object");
+  expect(typeof (listAll())).toBe("object");
 
   // Make assertions to check the expected behavior or outcomes
 
   // Clean up any test-specific resources or data
-  
+  AbortController.abort();
 });
