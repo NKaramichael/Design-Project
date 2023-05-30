debc7266c9d26ce34f27f0866c6d8625f3629420
Tristan Dos Remendos <tristan.remendos@gmail.com>
2023-05-30 23:26:28 +0200

    Update firebase-selectImages.js
    Co-Authored-By: SergioFrasco <128463412+SergioFrasco@users.noreply.github.com>
    Co-Authored-By: gabrieldrew <100573616+gabrieldrew@users.noreply.github.com>
    Co-Authored-By: nick-karamichael <127729535+nick-karamichael@users.noreply.github.com>

--- firebase/firebase-selectImages.js
+++ firebase/firebase-selectImages.js
@@ -1,16 +1,16 @@
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDPhBs6YrLXQspg8krTemU6WdlArx4lNQ4",
-//     authDomain: "pcgevaluation-49d75.firebaseapp.com",
-//     databaseURL: "https://pcgevaluation-49d75-default-rtdb.firebaseio.com",
-//     projectId: "pcgevaluation-49d75",
-//     storageBucket: "pcgevaluation-49d75.appspot.com",
-//     messagingSenderId: "369543877095",
-//     appId: "1:369543877095:web:84e7d5c5fdb84dd72eed42"
-//   };
+const firebaseConfig = {
+    apiKey: "AIzaSyDPhBs6YrLXQspg8krTemU6WdlArx4lNQ4",
+    authDomain: "pcgevaluation-49d75.firebaseapp.com",
+    databaseURL: "https://pcgevaluation-49d75-default-rtdb.firebaseio.com",
+    projectId: "pcgevaluation-49d75",
+    storageBucket: "pcgevaluation-49d75.appspot.com",
+    messagingSenderId: "369543877095",
+    appId: "1:369543877095:web:84e7d5c5fdb84dd72eed42"
+  };
   
-// // Initialise firebase
-// firebase.initializeApp(firebaseConfig);
+// Initialise firebase
+firebase.initializeApp(firebaseConfig);
 
 // let selectedImage;
 
