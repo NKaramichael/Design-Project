acfe87ebbe19e518c54578ab5b5c4914975a96d0
Tristan Dos Remendos <tristan.remendos@gmail.com>
2023-05-30 23:04:02 +0200

    Progress
    Co-Authored-By: SergioFrasco <128463412+SergioFrasco@users.noreply.github.com>
    Co-Authored-By: gabrieldrew <100573616+gabrieldrew@users.noreply.github.com>
    Co-Authored-By: nick-karamichael <127729535+nick-karamichael@users.noreply.github.com>

---
+++ firebase.test.js
@@ -0,0 +1,15 @@
+let {
+    myFirebaseFunction
+  } = require('./4-JavaScript/firebase-selectImages.js');
+  
+  test('Test myFirebaseFunction', async () => {
+    // Mock Firebase dependencies or initialize Firebase emulator
+    // to create a controlled testing environment
+  
+    // Call your Firebase function or execute the code that interacts with Firebase
+    
+  
+    // Make assertions to check the expected behavior or outcomes
+  
+    // Clean up any test-specific resources or data
+  });
