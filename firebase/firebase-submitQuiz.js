557d6212b220212450d1193d5743423d60746db5
Tristan Dos Remendos <tristan.remendos@gmail.com>
2023-06-02 09:41:11 +0200

    Quick touch

--- firebase/firebase-submitQuiz.js
+++ firebase/firebase-submitQuiz.js
@@ -126,37 +126,7 @@ async function uploadImages(files, imageArr) {
 }
 
 async function uploadImage(file, domain, model, imageNum) {
-    // try {
-    //     var storageRef = storage.ref().child('Level/images/' + file.name);
-
-    //     // Create the metadata object with custom metadata fields
-    //     var metadata = {
-    //         customMetadata: {
-    //             domain: domain,
-    //             model: model
-    //         }
-    //     };
-
-    //     // Upload the image file to Firebase Storage with metadata
-    //     await storageRef.put(file, metadata);
-    //     console.log('Image uploaded successfully!');
-
-    //     // Get the download URL of the uploaded image
-    //     var url = await storageRef.getDownloadURL();
-    //     console.log('Image URL:', url);
-
-    //     // Save the image URL and metadata in Cloud Firestore
-    //     var docRef = await firestore.collection('Levels').add({
-    //         imageUrl: url,
-    //         domain: domain,
-    //         model: model
-    //     });
-    //     const ref = 'ref' + imageNum;
-    //     sessionStorage.setItem(ref, docRef.id);
-    //     console.log('Image URL and metadata saved in Firestore! Ref:', docRef.id);
-    // } catch (error) {
-    //     console.log('Error uploading image:', error);
-    // }
+    
 }
 
 module.exports = { submitQuiz, change, submit};
